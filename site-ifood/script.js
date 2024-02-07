7a44b2b4f787a72c829590bb003a3bf087a12caf
Vinicius Correia Souza <154944076+CorreiaSouza@users.noreply.github.com>
2024-02-07 19:29:38 -0300

    07/02-1929

---
+++ site-ifood/script.js
@@ -0,0 +1,14 @@
+const menuDiv = document.getElementById('menu-mobile');
+const btnAnimar = document.getElementById('btn-menu');
+
+btnAnimar.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    menuDiv.classList.toggle('abrir');
+    btnAnimar.classList.toggle('ativar');
+}
+
+function animarMenu() {
+    const btn = document.getElementById('btn-menu');
+    btn.classList.toggle('ativar');
+}
